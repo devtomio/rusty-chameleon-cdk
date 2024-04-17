1c98c6a14c4993443b6f4b1aaab4b0ee9541f249
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2024-04-16 20:31:10 -0400

    fix lambda error response, update dependencies on rust function, use cdk rust construct, add rust utils

--- lib/rusty-chameleon-cdk-stack.ts
+++ lib/rusty-chameleon-cdk-stack.ts
@@ -1,9 +1,9 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
-import * as path from 'path';
-import {config} from 'dotenv';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { config } from "dotenv";
+import { RustFunction } from "cargo-lambda-cdk";
 
 export class RustyChameleonCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -11,23 +11,30 @@ export class RustyChameleonCdkStack extends Stack {
     config(); // config dotenv
 
     const pk = process.env.PUBLIC_KEY;
-    if(!pk) {
-      throw new Error("No Public Key Set in Env Vars")
+    if (!pk) {
+      throw new Error("No Public Key Set in Env Vars");
     }
-      // ? process.env.PUBLIC_KEY
-      // : throw new Error("Public key not set");
 
-    const chameleon = new lambda.Function(this, 'RustyChameleonlambda', {
-      runtime: Runtime.PROVIDED_AL2,
-      code: lambda.Code.fromAsset(path.join(__dirname, '../src/chameleon/target/lambda/chameleon/bootstrap.zip')),
-
-      handler: 'not.required',
-      environment: {
-        RUST_BACKTRACE: '1',
-        PUBLIC_KEY: pk, // will throw invalidStringLength error if not set
-        NASA_API_KEY: process.env.NASA_API_KEY || "",
+    const chameleon = new RustFunction(this, "RustyChameleonlambda", {
+      manifestPath: "src/chameleon/Cargo.toml",
+      bundling: {
+        environment: {
+          RUST_BACKTRACE: "1",
+          PUBLIC_KEY: pk, // will throw invalidStringLength error if not set
+          NASA_API_KEY: process.env.NASA_API_KEY || "",
+        },
       },
-      architecture: Architecture.X86_64,
     });
+
+    chameleon.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["ssm:GetParameter"],
+        resources: [
+          "arn:aws:ssm:us-east-1:*:parameter/rusty-chameleon/public-key",
+          "arn:aws:ssm:us-east-1:*:parameter/NASA_API_KEY",
+        ],
+        effect: cdk.aws_iam.Effect.ALLOW,
+      }),
+    );
   }
 }
